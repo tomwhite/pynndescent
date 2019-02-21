c92dc2d8a6c18d4d88d6aebdf9682f58512fbd0e
Tom White <tom.e.white@gmail.com>
2019-02-21 16:07:55 +0000

    Plot

--- benchmark_results2.py
+++ benchmark_results2.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 results = pd.read_csv('results/results_threads.csv')
 
-for algorithm, rows in (('pynndescent_regular', 50000), ('pynndescent_regular', 100000), ('pynndescent_threaded', 50000), ('pynndescent_threaded', 100000)):
+for algorithm, rows in (('pynndescent_regular', 100000), ('pynndescent_threaded', 100000), ('pynndescent_unsafe_threaded', 100000)):
     results_subset = results[(results['algorithm'] == algorithm) & (results['rows'] == rows)][['threads','duration']]
-    plt.plot('threads', 'duration', data=results_subset, marker='o', label='{} {} rows'.format(algorithm, rows))
+    plt.loglog('threads', 'duration', data=results_subset, marker='o', label='{} {} rows'.format(algorithm, rows))
 
 plt.title("Nearest neighbor algorithms (D=128, NN=25)")
 plt.xlabel('Threads')
